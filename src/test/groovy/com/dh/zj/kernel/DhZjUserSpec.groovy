bdb059b3ff456774fcced44ea85618d7ee3fd94e
hansh <111111>
2018-04-27 14:55:03 +0800

    创建角色实体

---
+++ src/test/groovy/com/dh/zj/kernel/DhZjUserSpec.groovy
@@ -0,0 +1,18 @@
+package com.dh.zj.kernel
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class DhZjUserSpec extends Specification implements DomainUnitTest<DhZjUser> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
