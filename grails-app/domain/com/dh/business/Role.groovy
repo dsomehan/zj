483cb9cc971b5139cca593687aaf64951560f23d
hansh <111111>
2018-05-15 14:20:40 +0800

    创建聊天实例

--- grails-app/domain/com/dh/business/Role.groovy
+++ grails-app/domain/com/dh/business/Role.groovy
@@ -1,9 +1,15 @@
 package com.dh.business
 
-class User {
+class Role {
     String id
-    String userName
-    String password
+    String roleName
+    int level=1
+    long exp=0
+    String country="新手"
+    int hp=50
+    int sp=10
+    int damage=20
+    int defense=10
     Date dateCreated
     Date lastUpdated
 
